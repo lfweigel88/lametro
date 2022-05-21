f1cae08460b3dc8d14af7f6cbef2adb4e30fa66b
Luke Weigel <lfweigel88@gmail.com>
2022-05-20 21:50:46 -0400

    lat long

--- cta/lametro.py
+++ cta/lametro.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
+#vehicle {
+#  trip {
+#    trip_id: "55987526"
+#    start_date: "20220520"
+#    route_id: "801"
+#  }
+#  position {
+#    latitude: 33.860725
+#    longitude: -118.21622
+#    bearing: 161.00594
+#  }
+#  current_stop_sequence: 14
+#  current_status: IN_TRANSIT_TO
+#  timestamp: 1653095509
+#  stop_id: "80109"
+#  vehicle {
+#    id: "1075-1098-1130"
+#    label: "1075-1098-1130"
+#  }
+#}
 import time
 import requests
 import os
@@ -12,5 +32,5 @@ headers = {'Authorization': lamkey}
 response = requests.get('https://api.goswift.ly/real-time/lametro-rail/gtfs-rt-vehicle-positions',headers=headers)
 feed.ParseFromString(response.content)
 for entity in feed.entity:
-	feeddict = feeddict + "{'"+entity.vehicle.trip.route_id+"': {'vehicle_id:' '"+entity.vehicle.vehicle.id+"' }"
+	feeddict = feeddict + "{'"+entity.vehicle.trip.route_id+"': {'vehicle_id:' '"+entity.vehicle.vehicle.id+"', 'latitude': '" + entity.vehicle.position.latitude +"', 'longitude': '" + entity.vehicle.position.longitude +"',} }"
 print(feeddict)

5ff494993efa40fc4ffa475e99c281b719a6d432
Luke Weigel <lfweigel88@gmail.com>
2024-11-30 12:14:54 -0500

    Fix marta duplicates

--- marta.py
+++ marta.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/var/www/html/lametro/venv/bin/python3
 import time
 import requests
 import os
@@ -10,18 +10,25 @@ feeddict = {}
 while True:
         response = requests.get('https://developerservices.itsmarta.com:18096/railrealtimearrivals?apiKey='+apikey,verify=False)
         i = 0
+        #print(response.json())
+        with open(homedir + 'marta2.json', 'w') as file:
+             file.write(json.dumps(response.json()))
         response = response.json()['RailArrivals']
+        #print(response)
         for entity in response:
-            feeddict[i] = {}
-            feeddict[i]['train_id'] = entity['TRAIN_ID']
-            feeddict[i]['latitude'] = entity['VEHICLELATITUDE']
-            feeddict[i]['longitude'] = entity['VEHICLELONGITUDE']
-            feeddict[i]['bearing'] = entity['DIRECTION']
-            feeddict[i]['next_stop'] = entity['STATION']
-            feeddict[i]['destination'] = entity['DESTINATION']
-            feeddict[i]['timestamp'] = entity['RESPONSETIMESTAMP']
-            feeddict[i]['line'] = entity['LINE']
-            i = i + 1
+            feeddict[entity['TRAIN_ID']] = {}
+            #print(entity['TRAIN_ID'])
+            #print(len(feeddict))
+            trainid = entity['TRAIN_ID']
+            feeddict[trainid]['train_id'] = entity['TRAIN_ID']
+            feeddict[trainid]['latitude'] = entity['VEHICLELATITUDE']
+            feeddict[trainid]['longitude'] = entity['VEHICLELONGITUDE']
+            feeddict[trainid]['bearing'] = entity['DIRECTION']
+            feeddict[trainid]['next_stop'] = entity['STATION']
+            feeddict[trainid]['destination'] = entity['DESTINATION']
+            feeddict[trainid]['timestamp'] = entity['RESPONSETIMESTAMP']
+            feeddict[trainid]['line'] = entity['LINE']
+            #i = i + 1
         with open(homedir + 'marta.json','w') as file:
-                file.write(json.dumps(feeddict))
+            file.write(json.dumps(feeddict))
         time.sleep(30)
